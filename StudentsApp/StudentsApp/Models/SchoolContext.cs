107f5e9664b557fd34ad8abf780ed3595798e376
pedroreal12 <pedrofilipereal12@gmail.com>
2024-01-17 10:51:57 +0000

    fix: changed the old connection string for the one in the container

--- StudentsApp/StudentsApp/Models/SchoolContext.cs
+++ StudentsApp/StudentsApp/Models/SchoolContext.cs
@@ -26,7 +26,7 @@ public partial class SchoolContext : DbContext
     public virtual DbSet<Role> Roles { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        => optionsBuilder.UseSqlServer("Server=.\\SQLEXPRESS;Database=School;Trusted_Connection=True;TrustServerCertificate=True;");
+        => optionsBuilder.UseSqlServer("Server=localhost, 1433;Database=School;User Id=SA;Password=A&VeryComplex123Password;TrustServerCertificate=True;MultipleActiveResultSets=true");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
